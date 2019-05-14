f5a28f5c6a6e3d85c9019a41cc4335c9cda73137
densongeorge <densongeorge18@gmail.com>
2019-05-15 04:42:52 +0530

    Python Server And Static Pages

---
+++ home.py
@@ -0,0 +1,200 @@
+import httplib2
+import os
+import oauth2client
+from oauth2client import client, tools
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+import mimetypes
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from oauth2client import file
+import json
+from flask import jsonify
+import random 
+import string
+
+
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Gmail API Python Send Email'
+
+def get_credentials():
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'gmail-python-email-send.json')
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def SendMessage(sender, to, subject, msgHtml, msgPlain, attachmentFile=None):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    if attachmentFile:
+        message1 = createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile)
+    else: 
+        message1 = CreateMessageHtml(sender, to, subject, msgHtml, msgPlain)
+    result = SendMessageInternal(service, "me", message1)
+    return result
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+        return "Error"
+    return "OK"
+
+def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    return {'raw': base64.urlsafe_b64encode(msg.as_string())}
+
+def createMessageWithAttachment(
+    sender, to, subject, msgHtml, msgPlain, attachmentFile):
+    """Create a message for an email.
+
+    Args:
+      sender: Email address of the sender.
+      to: Email address of the receiver.
+      subject: The subject of the email message.
+      msgHtml: Html message to be sent
+      msgPlain: Alternative plain text message for older email clients          
+      attachmentFile: The path to the file to be attached.
+
+    Returns:
+      An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart('mixed')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    messageA = MIMEMultipart('alternative')
+    messageR = MIMEMultipart('related')
+
+    messageR.attach(MIMEText(msgHtml, 'html'))
+    messageA.attach(MIMEText(msgPlain, 'plain'))
+    messageA.attach(messageR)
+
+    message.attach(messageA)
+
+    print("create_message_with_attachment: file: %s" % attachmentFile)
+    content_type, encoding = mimetypes.guess_type(attachmentFile)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(attachmentFile, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    filename = os.path.basename(attachmentFile)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    message.attach(msg)
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+
+def mail(content):
+    to = "h20180143@pilani.bits-pilani.ac.in"
+    to = content["email"]
+    #sender = "noreply.kycblockchain@gmail.com",
+    sender = "developerwani@gmail.com"
+    subject = "Account Created Successfully"
+    msgHtml = "<html><head><title>WelCome To KYC CHAIN</title></head><body><table><tr><td>Account Address</td><td>"+content['address']+"</td></tr><tr><td>Private Key</td><td>"+content['private_key']+"</td></tr></table></body></html>"
+    msgPlain = "Account Created Successfully"
+    SendMessage(sender, to, subject, msgHtml, msgPlain)
+    # Send message with attachment: 
+    #SendMessage(sender, to, subject, msgHtml, msgPlain, 'temp.py')
+
+#if __name__ == '__main__':
+#    main()
+
+from flask import Flask, request, render_template, session, flash, redirect, \
+    url_for, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/postjson',methods = ['POST'])
+def postjson():
+    print(request)
+    content = request.get_json()
+    #a = mail(content)
+    #print(a)
+    return "Successful Submit"
+    
+@app.route('/KYC')
+def KYC():
+    data = ""
+    with open('./build/contracts/KYC.json') as json_file:  
+    	data = json.load(json_file)
+    	return jsonify(data)    
+#return render_template('staff.html')
+
+@app.route('/registerUser')
+def registerUser():
+    return render_template('registerUser.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+@app.route('/postkey',methods = ['POST'])
+def postkey():
+    print(request)
+    content = request.get_json()
+    if content["address"] not in keyarr:
+        tempkey = randomString()
+        keyarr.update({content["address"] : tempkey})
+        return tempkey
+    else:
+        return keyarr[content["address"]]
+
+
+keyarr = {}
+if __name__ == '__main__':
+ #   app.run(host="0.0.0.0", debug=True)
+    app.run(host="172.18.16.108", debug=True)
+
